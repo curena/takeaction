44d7e2cb344f2f517844e81e4b24ce37ed34e06d
Can Pehlivanturk <pehlivanturk@utexas.edu>
2019-04-08 19:54:30 -0500

    Moved API key to environment variables

--- src/querySenators.py
+++ src/querySenators.py
@@ -1,12 +1,14 @@
 import urllib.request
-import config
+import os
 import json
 
 class SenatorInfo:
     def __init__ (self,state):
         url = "https://api.propublica.org/congress/v1/members/senate/" + state + "/current.json"
+        secret_key = os.environ.get("SECRET_KEY")
+        headers = {"X-API-Key": secret_key}
     
-        req = urllib.request.Request(url, headers = config.headers)
+        req = urllib.request.Request(url, headers = headers)
         resp = urllib.request.urlopen(req)
         data = resp.read()
         
@@ -14,7 +16,7 @@ class SenatorInfo:
     
     def getName(self, number):
         if (self.__data["status"] == "OK"):
-            return self.__data["results"][number]["name"] 
+            return self.__data["results"][number]["name"]
 
     def getTwitter(self,number):
         if (self.__data["status"] == "OK"):
