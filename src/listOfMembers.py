2d9e6d8707321f5342a8a9b41649f7b7f1957af8
Can Pehlivanturk <pehlivanturk@utexas.edu>
2019-04-09 02:48:27 -0500

    Added getSenatorPhoneNumber

--- src/listOfMembers.py
+++ src/listOfMembers.py
@@ -4,6 +4,7 @@ import json
 import io
 
 class ListOfMembers:
+
     def __init__ (self):
         url = "https://api.propublica.org/congress/v1/115/senate/members.json"
         secret_key = os.environ.get("SECRET_KEY")
@@ -13,15 +14,37 @@ class ListOfMembers:
         resp = urllib.request.urlopen(req)
         data = resp.read()
         
-        self.__data = json.loads(data)
+        self.__senateData = json.loads(data)
     
     def writeToFile(self):
         with io.open("data.txt", 'w') as f:
-            json.dump(self.__data, f, ensure_ascii=False, indent=2)
+            json.dump(self.__senateData, f, ensure_ascii=False, indent=2)
+    
+    def findStateInfo(self, state):
+        self.__stateData = []
+        for dict in self.__senateData["results"][0]["members"]:
+            if (dict["state"] == state):
+                self.__stateData.append(dict)
 
+    def printStateData(self):
+        print(self.__stateData)
+    
+    def getSenatorFirstName(self,number):
+        return self.__stateData[number]["first_name"]
 
+    def getSenatorMiddleName(self,number):
+        return self.__stateData[number]["middle_name"]
+
+    def getSenatorLastName(self,number):
+        return self.__stateData[number]["last_name"]
+
+    def getSenatorPhoneNumber(self,number):
+        return self.__stateData[number]["phone"]
 
 if __name__ == "__main__":
     #Print to file
     myList = ListOfMembers()
-    myList.writeToFile()
+    myList.findStateInfo("TX")
+    myList.printStateData()
+    print(myList.getSenatorFirstName(0))
+    print(myList.getSenatorPhoneNumber(0))
